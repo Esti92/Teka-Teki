dacd6f582fabe62f3c2e81645b29476c9d5c0041
YERAY <YERAY@YERAY-PC>
2016-03-21 18:40:20 +0100

    ventana registro

--- Teka-Teki/src/ventanas/VRegistro.java
+++ Teka-Teki/src/ventanas/VRegistro.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 public class VRegistro extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtNombre;
+	private JTextField txtCorreo;
+	private JTextField txtPassword;
+	private JLabel lblTerminos;
 
 	/**
 	 * Launch the application.
@@ -36,8 +46,38 @@ public class VRegistro extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[][][][228.00,grow][25.00][31.00][]", "[][35.00][35.00][35.00][35.00][35.00]"));
+		
+		txtNombre = new JTextField();
+		txtNombre.setForeground(Color.LIGHT_GRAY);
+		txtNombre.setText("Nombre de usuario");
+		contentPane.add(txtNombre, "cell 3 1,grow");
+		txtNombre.setColumns(10);
+		
+		txtCorreo = new JTextField();
+		txtCorreo.setForeground(Color.LIGHT_GRAY);
+		txtCorreo.setText("E-mail");
+		contentPane.add(txtCorreo, "cell 3 2,grow");
+		txtCorreo.setColumns(10);
+		
+		txtPassword = new JTextField();
+		txtPassword.setForeground(Color.LIGHT_GRAY);
+		txtPassword.setText("Password");
+		contentPane.add(txtPassword, "cell 3 3,grow");
+		txtPassword.setColumns(10);
+		
+		JLabel lblClicandoEncrear = new JLabel("Clicando en \"Crear mi cuenta\" aceptas los");
+		contentPane.add(lblClicandoEncrear, "flowx,cell 3 4");
+		
+		JButton btnRegistrar = new JButton("Crear mi cuenta");
+		btnRegistrar.setForeground(Color.BLACK);
+		btnRegistrar.setFont(new Font("Century Gothic", Font.PLAIN, 15));
+		contentPane.add(btnRegistrar, "cell 3 5,grow");
+		
+		lblTerminos = new JLabel("Terminos y Condiciones");
+		lblTerminos.setForeground(Color.BLUE);
+		contentPane.add(lblTerminos, "cell 3 4");
 	}
 
 }
